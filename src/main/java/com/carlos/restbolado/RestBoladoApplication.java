66450449317cf0160d1168f393e2dda613d8f4e8
CarlosBotelho42 <carlospiece55@gmai.com>
2023-05-12 10:52:06 -0300

    Hello

---
+++ src/main/java/com/carlos/restbolado/RestBoladoApplication.java
@@ -0,0 +1,13 @@
+package com.carlos.restbolado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestBoladoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestBoladoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlos/restbolado/RestBoladoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlos.restbolado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestBoladoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
